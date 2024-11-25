1712eeb69847af1b772054f772d8e1d289f74dc0
Jake Clark <jclark@ignitesales.com>
2024-11-25 01:52:03 -0600

    fix validation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,17 +2,27 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 import { routes } from './app.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PrimeNGModule } from './shared/primeng.module';
 import { ObjectManagerModule } from './modules/object-manager/object-manager.module';
+import { ObjectSchemaService } from './services/object-schema.service';
+import { ObjectInstanceService } from './services/object-instance.service';
+import { ObjectMigrationService } from './services/object-migration.service';
+import { GeneratedObjectsService } from './services/generated-objects.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(),
+    MessageService,
+    ObjectSchemaService,
+    ObjectInstanceService,
+    ObjectMigrationService,
+    GeneratedObjectsService,
     importProvidersFrom([
       FormsModule,
       ReactiveFormsModule,
