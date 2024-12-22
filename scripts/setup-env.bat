d2cb063076f77b20e6e6bff584776b0ce4c0c030
nascarjake <nascarjake@gmail.com>
2024-12-21 21:41:47 -0600

    env setup files tweak paths

--- scripts/setup-env.bat
+++ scripts/setup-env.bat
@@ -3,13 +3,13 @@ setlocal
 
 echo Setting up environment files...
 
-if not exist ..\src\environments\environment.ts (
-    copy ..\src\environments\environment_public.ts ..\src\environments\environment.ts
+if not exist .\src\environments\environment.ts (
+    copy .\src\environments\environment_public.ts .\src\environments\environment.ts
     echo Created environment.ts from public template
 )
 
-if not exist ..\src\environments\environment.development.ts (
-    copy ..\src\environments\environment_public.ts ..\src\environments\environment.development.ts
+if not exist .\src\environments\environment.development.ts (
+    copy .\src\environments\environment_public.ts .\src\environments\environment.development.ts
     echo Created environment.development.ts from public template
 )
 
--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Copy the public environment file as a template if local environment doesn't exist
-if [ ! -f ../src/environments/environment.ts ]; then
-    cp ../src/environments/environment_public.ts ../src/environments/environment.ts
+if [ ! -f ./src/environments/environment.ts ]; then
+    cp ./src/environments/environment_public.ts ./src/environments/environment.ts
     echo "Created environment.ts from public template"
 fi
 
-if [ ! -f ../src/environments/environment.development.ts ]; then
-    cp ../src/environments/environment_public.ts ../src/environments/environment.development.ts
+if [ ! -f ./src/environments/environment.development.ts ]; then
+    cp ./src/environments/environment_public.ts ./src/environments/environment.development.ts
     echo "Created environment.development.ts from public template"
 fi
 
